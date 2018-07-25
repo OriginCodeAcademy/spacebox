2cba4fd1a02c5f4cd90fb48509de4b3de7feb6bf
Michael Roberts Jr <michael.e.roberts.jr@gmail.com>
2018-07-24 18:25:23 -0700

    Creates env vars for auth and artist route, authenticates with Spotify (#1)

--- server/index.js
+++ server/index.js
@@ -2,13 +2,41 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var SpotifyWebApi = require('spotify-web-api-node');
+require('dotenv').config();
 
 var songs = [];
 
+let accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
+let refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
+const spotifyApi = new SpotifyWebApi({ 
+  clientId: process.env.SPOTIFY_CLIENT_ID, 
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: 'http://localhost:8080/',
+  accessToken,
+  refreshToken 
+});
+const refreshSpotifyToken = () => spotifyApi.refreshAccessToken()
+                .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+                .catch(err => console.log(err))();
+
+const handShake = () => {
+  //Retrieves authorizationURL which prompts login
+  var authorizationUrl = spotifyApi.createAuthorizeURL(['playlist-read-private', 'playlist-modify-private', 'streaming', 'app-remote-control', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-state'], 'random string')
+  // Using code in response headers, requests a token
+  spotifyApi.authorizationCodeGrant(process.env.CODE)
+    .then(data => {
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+      }
+    )
+    .catch(err => console.log(err))
+}
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,POST');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  next();
 });
 app.use(express.json());
 app.use('/', express.static('build'));
@@ -16,27 +44,39 @@ app.use('/', express.static('public'));
 
 
 //Validation checks
-app.post('*', (req,res,next) => {
+app.post('*', (req, res, next) => {
 
   next();
 })
 
 //Main pages
-app.get('/', (req,res) => res.render('index', { songs }));
-app.get('/board', (req,res) => res.json(songs));
+app.get('/', (req, res) => res.render('index', { songs }));
+app.get('/board', (req, res) => res.json(songs));
+
 
 //Register
-app.post('/request', (req,res) => {
+app.post('/request', (req, res) => {
   songs.push([]);
   io.emit('update', songs);
   res.json({ message: `Thanks` });
 });
 
 //Custom routes
-app.get('/404', (req,res) => res.json({ message: 'Nothing is here. But thanks for checking!' }));
+app.get('/404', (req, res) => res.json({ message: 'Nothing is here. But thanks for checking!' }));
+
+app.get('/artist/:artist', (req,res) => {
+  // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
+  spotifyApi.searchArtists(req.params.artist)
+  .then(data => {
+    const items = data.body.artists.items;
+    if (!items.length) res.send('Stop it');
+    res.send(items[0])
+  })
+  .catch(err => console.log(err));
+})
 
 //Deleting teams
-app.delete('/request', (req,res,next) => {
+app.delete('/request', (req, res, next) => {
   const newSongs = songs.filter(t => t.name != req.body.name);
   songs = newSongs;
   io.emit('update', songs);
