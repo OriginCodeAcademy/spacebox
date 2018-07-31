4f979bf08047bee6345c35196ba3efb99c564177
Michael Roberts <michaelroberts@Michaels-MacBook-Air.local>
2018-07-30 18:19:29 -0700

    Initializes token to null & refreshes if needed, adds prefix to routes

--- server/index.js
+++ server/index.js
@@ -7,22 +7,18 @@ require('dotenv').config();
 
 var songs = [];
 
-let accessToken = process.env.SPOTIFY_ACCESS_TOKEN;
+let accessToken = null;
 let refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
 const spotifyApi = new SpotifyWebApi({ 
   clientId: process.env.SPOTIFY_CLIENT_ID, 
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   redirectUri: 'http://localhost:8080/',
-  accessToken,
   refreshToken 
 });
-const refreshSpotifyToken = () => spotifyApi.refreshAccessToken()
-                .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-                .catch(err => console.log(err))();
 
 const createAuthorizationUrl = () => {
   //Retrieves authorizationURL which prompts login
-  return spotifyApi.createAuthorizeURL(['playlist-read-private', 'playlist-modify-private', 'streaming', 'app-remote-control', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-state'], 'random string')
+  return spotifyApi.createAuthorizeURL(['playlist-read-private', 'playlist-modify-private', 'streaming', 'app-remote-control', 'user-modify-playback-state', 'user-read-currently-playing', 'user-read-playback-state'], 'random-string')
 }
 const getTokens = () => {
     // Using code in response headers, requests a token
@@ -73,7 +69,26 @@ app.post('/request', (req, res) => {
 //Custom routes
 app.get('/404', (req, res) => res.json({ message: 'Nothing is here. But thanks for checking!' }));
 
-app.get('/artist/:artist', (req,res) => {
+app.use('/api', (req, res, next) => {
+
+  const lastTime = new Date();
+  const currentTime = new Date();
+  
+  if (lastTime < currentTime || !accessToken) {
+    spotifyApi.refreshAccessToken()
+      .then(data => {
+            lastTime.setSeconds(data.body.expires_in);
+            accessToken = data.body['access_token'];
+            spotifyApi.setAccessToken(data.body['access_token']);
+            next();
+      })
+      .catch(err => console.log(err))
+  } else {
+    next();
+  }
+});
+
+app.get('/api/artist/:artist', (req,res) => {
   // clientId, clientSecret and refreshToken has been set on the api object previous to this call.
   spotifyApi.searchArtists(req.params.artist)
   .then(data => {
@@ -84,7 +99,7 @@ app.get('/artist/:artist', (req,res) => {
   .catch(err => console.log(err));
 })
 
-app.get('/playlist', (req, res) => {
+app.get('/api/playlist', (req, res) => {
   spotifyApi.getPlaylist('jy0ambrgj79gi3vw9ndg4qxlf', '25wrjLz6FFIPS4tnwartfC')
   .then(data => {
     const playlistInfo = {
@@ -110,7 +125,7 @@ app.get('/playlist', (req, res) => {
 // });
 
 //Deleting teams
-app.delete('/request', (req, res, next) => {
+app.delete('/api/request', (req, res, next) => {
   const newSongs = songs.filter(t => t.name != req.body.name);
   songs = newSongs;
   io.emit('update', songs);
