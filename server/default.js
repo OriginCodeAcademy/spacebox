dd03ba4cbd3ae55192a50468215479101917f9d9
Anthony Valera <anthony@anthonyvalera.com>
2018-08-28 13:20:20 -0700

    Queues default songs, ensures no duplicates, and starts jukebox when off

---
+++ server/default.js
@@ -0,0 +1,57 @@
+module.exports = [{
+    id: '299vmLW2iaQxe8y9HLWNiH',
+    name: 'just ask',
+    artist: 'weird inside',
+    albumCover: 'https://i.scdn.co/image/6b440d0d81145350b4bf87c7a77d679701dd4f22',
+    duration: 173846,
+    uri: 'spotify:track:299vmLW2iaQxe8y9HLWNiH'
+  },
+  {
+    id: '5HE2u1IOizZwM4kWMF5Jpb',
+    name: 'Sunset',
+    artist: 'Coubo',
+    albumCover: 'https://i.scdn.co/image/2362328b20a356c1d23897d38531b1b93844c788',
+    duration: 213361,
+    uri: 'spotify:track:5HE2u1IOizZwM4kWMF5Jpb'
+  },
+  {
+    id: '4uZ6QSZJPzCD4d7Jtscwes',
+    name: 'Minutes',
+    artist: 'Freddie Joachim',
+    albumCover: 'https://i.scdn.co/image/786f2d9d6117cb1f7262e835c43850237444259a',
+    duration: 227186,
+    uri: 'spotify:track:4uZ6QSZJPzCD4d7Jtscwes'
+  },
+  {
+    id: '2GPKo5nrX2uwB8eSCebIIk',
+    name: 'sincerely, yours',
+    artist: 'Nohidea',
+    albumCover: 'https://i.scdn.co/image/96b8069a0d544d1111f65bb37da3fb05f7b59b54',
+    duration: 139684,
+    uri: 'spotify:track:2GPKo5nrX2uwB8eSCebIIk'
+  },
+  {
+    id: '0ej5zbkF48ZiPZOubIX1e1',
+    name: 'Days To Come - Instrumental',
+    artist: 'Bonobo',
+    albumCover: 'https://i.scdn.co/image/fe00d367ac66e49eae8a8783bb9c36779ccee613',
+    duration: 230400,
+    uri: 'spotify:track:0ej5zbkF48ZiPZOubIX1e1'
+  },
+  {
+    id: '1pWAOjPUV6rrGcmLJaXgPR',
+    name: 'On the Dub-ble',
+    artist: 'DJ Krush',
+    albumCover: 'https://i.scdn.co/image/cb834f2adc55feb0dc19ec3e4380f7e8e797a368',
+    duration: 121280,
+    uri: 'spotify:track:1pWAOjPUV6rrGcmLJaXgPR'
+  },
+  {
+    id: '3m9X1ZYQPKKZXZpykC2jBf',
+    name: 'Whispers',
+    artist: 'Freddie Joachim',
+    albumCover: 'https://i.scdn.co/image/aea57c1c3d68e1dfb5c01aaeacd2bad0c0a2927c',
+    duration: 133799,
+    uri: 'spotify:track:3m9X1ZYQPKKZXZpykC2jBf'
+  }
+]
