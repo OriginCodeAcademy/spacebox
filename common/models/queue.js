9599822a9348cb7528e34946aa6acb5a419e8a04
Tiana Hayden <tiana.hayden@me.com>
2018-10-26 13:41:03 -0700

    created remote method for getplaylist that returns a list of song tracks currently on the playlist

--- common/models/queue.js
+++ common/models/queue.js
@@ -1,47 +1,88 @@
 'use strict';
 
 module.exports = function (Queue) {
-    // queue.getPlaylist = function (id, cb) {
-    //     // finds the correct queue if there are mutliple
-    //     queue.find({
-    //         where: {
-    //             id: id
-    //         }
-    //     })
-    //         .then((queue) => {
-    //             spotifyApi.getPlaylist(process.env.SPOTIFY_USER, process.env.SPOTIFY_PLAYLIST)
-    //                 .then(data => {
-    //                     if (queue.songs.length !== 0) {
-    //                         const playlistInfo = {
-    //                             url: data.body.external_urls.spotify,
-    //                             image: data.body.images[0].url,
-    //                             tracks: data.body.tracks.items.map(i => formatSong(i.track))
-    //                         }
-    //                         cb(null, playlistInfo.tracks)
-    //                     }
-    //                     else cb(null, [])
-    //                 })
-    //                 .catch(err => console.log(err));
-    //         })
-    //         .catch(err => console.log(err));
-    // }
+    Queue.getPlaylist = function (id, cb) {
+        var user = Queue.app.models.user;
+        // finds the correct queue based on the queue ID that you put in
+        Queue.findById(id, { fields: { userId: true } })
+            .then((queue) => {
+                var userID = queue.userId;
+                // takes the userID from the queue and gets spotifyID and playlistID from that user
+                user.findById(userID)
+                    .then((user) => {
+                        console.log(user.refreshToken)
+                        const SpotifyWebApi = require('spotify-web-api-node');
+                        const spotifyApi = new SpotifyWebApi({
+                            clientId: process.env.SPOTIFY_CLIENT_ID,
+                            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+                            redirectUri: process.env.SITE_URL || 'http://localhost:8080',
+                            refreshToken: user.refreshToken
+                        });
+                        var spotifyID = user.spotifyID
+                        var playlistID = user.playlistID
+                        spotifyApi.refreshAccessToken()
+                            .then(data => {
+                                // lastTime = new Date();
+                                // lastTime.setSeconds(data.body.expires_in);
+                                // accessToken = data.body['access_token'];
+                                spotifyApi.setAccessToken(data.body['access_token']);
+                                spotifyApi.getPlaylist(spotifyID, playlistID)
+                                    .then(playlist => {
+                                        const formatSong = track => ({
+                                            id: track.id,
+                                            name: track.name,
+                                            artist: track.artists[0].name,
+                                            albumCover: track.album.images[0].url,
+                                            duration: track.duration_ms,
+                                            uri: track.uri
+                                          })
+                                        var tracks = playlist.body.tracks.items.map(i => formatSong(i.track))
+                                        cb(null, tracks);
+                                    })
+                                    .catch(err => cb(err));
+                            })
+                            .catch(err => console.log(err))
+                    })
+                // find user, get playlist id and spotify id and 
+                // then using those, get the playlist from spotify api,
+                // then modify the songsids instead of the queue
 
-    // queue.remoteMethod('getPlaylist', {
-    //     description: 'Gets current playlist from Spotify before.',
-    //     accepts: {
-    //         arg: 'id',
-    //         type: 'string'
-    //     },
-    //     http: {
-    //         path: '/getPlaylist',
-    //         verb: 'get'
-    //     },
-    //     returns: {
-    //         arg: 'data',
-    //         type: 'array',
-    //         root: true
-    //     },
-    // });
+                // find playlist id instead of user
+                // spotifyApi.getPlaylist(process.env.SPOTIFY_USER, process.env.SPOTIFY_PLAYLIST)
+                // gets the playlist on spotify using the users spotifyID and playlist ID
+                // spotifyApi.getPlaylist(user.spotifyID, user.playlistID)
+                //     .then(data => {
+                //         if (queue.songsIDs.length !== 0) {
+                //             const playlistInfo = {
+                //                 url: data.body.external_urls.spotify,
+                //                 image: data.body.images[0].url,
+                //                 tracks: data.body.tracks.items.map(i => formatSong(i.track))
+                //             }
+                //             cb(null, playlistInfo.tracks)
+                //         }
+                //         else cb(null, [])
+                //     })
+                //     .catch(err => console.log(err));
+            })
+            .catch(err => cb(err));
+    }
+
+    Queue.remoteMethod('getPlaylist', {
+        description: 'Gets current playlist from Spotify',
+        accepts: {
+            arg: 'id',
+            type: 'string'
+        },
+        http: {
+            path: '/getPlaylist',
+            verb: 'get'
+        },
+        returns: {
+            arg: 'data',
+            type: 'array',
+            root: true
+        },
+    });
 
 
     // queue.updatePlaylist = async function (id, cb) {
