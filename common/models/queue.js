5c35ecdd6c0c4e6177f4a477e2441ab9547efa5f
tianahayden <tiana.hayden@me.com>
2018-10-29 10:12:15 -0700

    Tmh sbox 9 (#18)
    * fixed user relation model so there is one queue for every user
    
    * created remote method for getplaylist that returns a list of song tracks currently on the playlist
    
    * created updatePlaylist remote method that works to get current playlist > remove current playing song > add new song > update spotify playlsit to reflect those changes. Next, I need to get it to add default songs when the playlist only has one song left, and I need to track last played songs, and I need to make this remote method a post request to update the queue field in the queue model
    
    * simplifed updatePlaylist by using getPlaylist within it
    
    * fixed authorization issues so others can authorize by going to localhost/spotify

--- common/models/queue.js
+++ common/models/queue.js
@@ -1,5 +1,59 @@
 'use strict';
+const { getPlaylist, updatePlaylist } = require('../../server/utils/playlist')
 
-module.exports = function(Queue) {
+module.exports = function (Queue) {
+    Queue.getPlaylist = function (id, cb) {
+        getPlaylist(id)
+            .then((tracks) => cb(null, tracks))
+            .catch(err => cb(err));
+    }
 
+    Queue.remoteMethod('getPlaylist', {
+        description: 'Gets current playlist from Spotify',
+        accepts: {
+            arg: 'id',
+            type: 'string'
+        },
+        http: {
+            path: '/getPlaylist',
+            verb: 'get'
+        },
+        returns: {
+            arg: 'data',
+            type: 'array',
+            root: true
+        },
+    });
+
+    Queue.updatePlaylist = function (id, songID, cb) {
+        updatePlaylist(id, songID)
+            .then((tracks) => {
+                // Queue.upsert({id: id}, {songIds: tracks})
+                cb(null, tracks)
+            })
+            .catch(err => cb(err));
+    }
+
+
+
+    Queue.remoteMethod('updatePlaylist', {
+        description: 'Adds new song to playlist and removes current playing song from playlist. Adds default songs if needed.',
+        accepts: [{
+            arg: 'id',
+            type: 'string'
+        },
+        {
+            arg: 'songID',
+            type: 'string'
+        }],
+        // http: {
+        //     path: '/updatePlaylist',
+        //     verb: 'post'
+        // },
+        returns: {
+            arg: 'data',
+            type: 'array',
+            root: true
+        },
+    });
 };
