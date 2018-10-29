3aaf7b2c48cdbef4837fb5f2edf8512d4873f060
Alex Barclay <adbarclay92@gmail.com>
2018-10-29 11:58:48 -0700

    Rebased

--- src/components/DefaultQueue/DQueueReducer.js
+++ src/components/DefaultQueue/DQueueReducer.js
@@ -5,6 +5,11 @@ const initialState = {
 function DQueueReducer(state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
-    case 'REMOVE_SONG'
+    case 'REMOVE_SONG_FULFILLED': {
+      return state;
+    }
+    default: {
+      return state;
+    }
   }
 }
--- src/components/SongQueue/SongQueue.jsx
+++ src/components/SongQueue/SongQueue.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 export default class SongQueue extends Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		return(
-			<div>Song Queue</div>
-		)
-	}
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Song Queue</div>
+    )
+  }
 }
