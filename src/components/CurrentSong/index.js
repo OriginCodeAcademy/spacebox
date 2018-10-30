d88c1ed152abfa6fd4fe93410d3ccd9f44855dba
Austin <austin.gray4292@gmail.com>
2018-10-30 12:05:12 -0700

    fixes admin dashboard errors

--- src/components/CurrentSong/index.js
+++ src/components/CurrentSong/index.js
@@ -3,10 +3,10 @@ import CurrentSong from './CurrentSong';
 
 function mapStoreToProps(store) {
   return {
-    song: store.Song.song,
-    artist: store.Song.artist,
-    length: store.Song.length,
-    playing: store.Song.playing
+    // song: store.Song.song,
+    // artist: store.Song.artist,
+    // length: store.Song.length,
+    // playing: store.Song.playing
   };
 }
 
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -4,6 +4,7 @@ import SearchReducer from './components/Search/SearchReducer';
 import DQueueReducer from './components/DefaultQueue/DQueueReducer';
 import SQueueReducer from './components/SongQueue/SQueueReducers';
 import SongReducer from './components/CurrentSong/SongReducer';
+import LoginReducer from './components/Login/LoginReducer'
 
 const rootReducer = combineReducers({
   HomePage: HomePageReducer,
