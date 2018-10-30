2c86d45c933d60994376408af58da9e7a092b95a
Andrew Prolman <aprolman21@gmail.com>
2018-10-30 09:44:17 -0700

    adds login test and styling

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { updateUsername, updatePassword } from './LoginActions'
+import { Redirect } from 'react-router';
+import { updateUsername, updatePassword, postLogin } from './LoginActions'
 
 export default class Login extends Component {
 	constructor(props) {
         super(props);
         this.handleUsername = this.handleUsername.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
+        this.submitLogin = this.submitLogin.bind(this);
     }
 
     handleUsername(e){
@@ -20,9 +22,14 @@ export default class Login extends Component {
 
     submitLogin(e){
         e.preventDefault();
+        const { dispatch, username, password } = this.props;
+        dispatch(postLogin({ username, password }));
     }
 
     render(){
+        if(this.props.token) {
+            return <Redirect push to='/dashboard' />
+        }
         return(
             <div className='container'>
                 <div>
