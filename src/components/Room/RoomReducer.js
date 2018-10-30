608dd3d1dd7de7e2a53a5ef7232f19913fea8474
Vance Pope <vancepope@outlook.com>
2018-10-30 10:04:40 -0700

    Added socket.io

--- src/components/Room/RoomReducer.js
+++ src/components/Room/RoomReducer.js
@@ -3,10 +3,17 @@ const initialState = {
   queueId: '',
 };
 
+<<<<<<< HEAD
 export default function RoomReducer(state = initialState, action) {
   const {type, payload} = action;
   switch (type) {
     case 'UPDATE_ROOM_TRACKS':
+=======
+function RoomReducer(state = initialState, action) {
+  const {type, payload} = action;
+  switch (type) {
+    case 'UPDATE_ROOM':
+>>>>>>> Added socket.io
       return {
         ...state,
         queueId: payload
