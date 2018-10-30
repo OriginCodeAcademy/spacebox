964012a94641f582fa32ba6b6eb48995e1b57eda
Alex Barclay <adbarclay92@gmail.com>
2018-10-30 14:38:23 -0700

    Adds initial play button, centered correctly

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -42753,3 +42754,5 @@ module.exports = function (css) {
 /***/ })
 /******/ ]);
 //# sourceMappingURL=bundle.js.map
+=======
+>>>>>>> Adds initial play button, centered correctly
