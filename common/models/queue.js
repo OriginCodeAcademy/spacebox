f52ff0936bfbb5d98df153eb3db7fb0372b92d97
Alex Barclay <adbarclay92@gmail.com>
2018-10-31 21:39:20 -0700

    Fixes accidental removal

--- common/models/queue.js
+++ common/models/queue.js
@@ -58,6 +58,37 @@ module.exports = function (Queue) {
     },
   });
 
+  Queue.addToDefaultSongs = function (id, uri, cb) {
+    addToDefaultSongs(id, uri)
+      .then((queue) => {
+        Queue.replaceOrCreate(queue)
+        cb(null, queue)
+      })
+      .catch(err => cb(err));
+  };
+
+  Queue.remoteMethod('addToDefaultSongs', {
+    description: 'Adds new song to default song array',
+    accepts: [
+      {
+        arg: 'id',
+        type: 'string'
+      },
+      {
+        arg: 'uri',
+        type: 'string'
+      }],
+    http: {
+      path: '/addToDefaultSongs',
+      verb: 'post'
+    },
+    returns: {
+      arg: 'data',
+      type: 'array',
+      root: true
+    },
+  });
+
   Queue.pauseCurrentSong = function (id, cb) {
     pauseCurrentSong(id)
       .then(response => cb(null, response))
