c6908f1f9d42a0483a64e78b236fcfc5e438ea2c
Alex Barclay <adbarclay92@gmail.com>
2018-10-31 21:24:36 -0700

    Adds completed testing. Ticket completed

--- tests/playback.spec.js
+++ tests/playback.spec.js
@@ -4,10 +4,43 @@ const React = require('react');
 const expect = require('chai').expect;
 const Nightmare = require('nightmare');
 const server = require('../server/server');
-const url = 'http://localhost:3000/';
+const url = 'http://localhost:3000/#';
 
 let nightmare;
 
 server.listen(3000);
 
-describe('CurrentSong')
+describe('Playback Button', () => {
+  beforeEach(() => {
+    nightmare = Nightmare({
+      show: true
+    })
+  });
+
+  it('should exist', function (done) {
+    this.timeout(10000);
+    nightmare
+      .goto(url + '/dashboard')
+      .wait(1000)
+      .evaluate(() => document.querySelector('#play-btn-circle').id)
+      .end()
+      .then(id => {
+        expect(id).to.equal('play-btn-circle')
+        done()
+      })
+  });
+
+  it('should alter playing on-click', function () {
+    nightmare
+      .goto(url + '/dashboard')
+      .wait(3000)
+      .click('div[name=play]')
+      .wait(3000)
+      .evaluate(() => document.querySelector('.play-pause').value)
+      .end()
+      .then(value => {
+        expect(value).to.equal('true')
+        done()
+      })
+  });
+})
